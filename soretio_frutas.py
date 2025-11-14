5ea509bfbcaf5064ba355b188c249a9fdc06f475
gabriela peratello <gabriela.p.silva10@aluno.senai.br>
2025-11-14 15:00:30 -0300

    add: quase finalizado

--- soretio_frutas.py
+++ soretio_frutas.py
@@ -10,6 +10,10 @@ frutas = ["🍒", "🍎", "🍓", "🍅"]
 class Sorteio_frutas:
     def __init__(self):
         self.janela = ttk.Window(themename="cosmo")
+
+        self.janela.title("Jogo de sorte - Frutas")
+        self.janela.iconbitmap("fruits.ico")
+
         self.janela.geometry("700x600")
         self.janela.resizable(False, False)
 
@@ -35,39 +39,65 @@ class Sorteio_frutas:
         frame_botoes.pack(pady=20)
 
         
-        self.janela_botao = ttk.Button(frame_botoes, style="outline button", text="Sortear", width=20, command=sortear)
+        self.janela_botao = ttk.Button(frame_botoes, style="outline button", text="Sortear", width=20, command=self.sortear)
         self.janela_botao.pack(side="left", padx=10)
 
 # ------------------------------------------------------------------------------------------------------------------------------------
         #TITULO
-        self.janela_resultados = ttk.Label(self.janela, text="Se liga nos seus resultados!", font=("sylfaen", 15)).pack(pady=5)
+        self.janela_resultados = ttk.Label(self.janela, text="Se liga nos seus resultados:", font=("sylfaen", 15)).pack(pady=5)
         #TREEVIEW
         self.tw = ttk.Treeview(self.janela)
         self.tw.pack(padx=15)
         #NÃO EH O NOME QUE APARECE NA TELA
         self.tw["columns"] = ("resultado")
         #CABEÇALHO
-        self.tw.heading("resultado", text="resultado")
+        self.tw.heading("resultado", text="Resultados anteriores")
         self.tw["show"] = "headings"
 
+        self.banco_dados()
+
+
 #--------------------------------------------------------------------------------------------------------------------------------------        
-        def sortear():
-                f01 = random.choice(frutas)
-                f02 = random.choice(frutas)
-                f03 = random.choice(frutas)
+    def sortear(self):
+        f01 = random.choice(frutas)
+        f02 = random.choice(frutas)
+        f03 = random.choice(frutas)
 
-                # 2. Atualizar Frontend (Labels)
-                self.fruta01.config(text=f01)
-                self.fruta02.config(text=f02)
-                self.fruta03.config(text=f03)
+        # 2. Atualizar Frontend (Labels)
+        self.fruta01.config(text=f01)
+        self.fruta02.config(text=f02)
+        self.fruta03.config(text=f03)
 
-                if f01 == f02 and f02 == f03 :
-                        messagebox.showinfo("Parabéns, você ganhou!", 
-                                        f"\n\nVocê conseguiu {f01}-{f02}-{f03}!")
+        if f01 == f02 and f02 == f03 :
+                messagebox.showinfo("Parabéns, você ganhou!", 
+                        f"Você conseguiu {f01}-{f02}-{f03}!")
 
-               
-                # jogada = f"{f01} - {f02} - {f03}"
+     
 
+
+    def banco_dados(self):
+         
+         conexao = sqlite3.connect("sorteio.bd")
+         cursor = conexao.cursor()
+
+         cursor.execute(""" CREATE TABLE IF NOT EXISTS tentativas (
+                        resultado TEXT NOT NULL)""")
+         conexao.commit()
+         conexao.close()
+
+    def atualizar(self):
+         #FAZER SELECT
+         conexao =sqlite3.connect("sorteio.db")
+         cursor = conexao.cursor()
+
+         sql = "SELECT resultado FROM tentavivas"
+         cursor.execute(sql)
+         resultados = cursor.fetchall()
+         conexao.close()
+
+         #LIMPAR O TREEVIEW
+         for a in self.tree.get_children():
+              self.tree.insert
 
 
 
